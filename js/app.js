cb6c19bb4ec62f663a0dacf467f2bfda99f87bcb
LimboLam <liam.xiong24@student.cart.org>
2025-04-03 10:30:38 -0700

    stuff

--- js/app.js
+++ js/app.js
@@ -6,3 +6,22 @@ document.addEventListener("DOMContentLoaded", function () {
         extraInfo.classList.toggle("hidden");
     });
 });
+
+let endArray = [];
+
+function storeChoice() {
+    let buttonName = this.name;
+    console.log("Button name:", buttonName);
+    endArray.push(buttonName);
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener("click", storeChoice);
+});
+
+function returnChoices() {
+    const arrayContainer = document.getElementById("arrayContainer");
+
+    arrayContainer.innerHTML = valuesString;
+}
