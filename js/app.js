4271c33fe201da42a5cd5a3f5b6c4ba81161cda2
LimboLam <liam.xiong24@student.cart.org>
2025-04-04 10:32:22 -0700

    JS

--- js/app.js
+++ js/app.js
@@ -7,12 +7,32 @@
 //     });
 // });
 
-let data = sessionStorage.getItem("buttonName");
+let sessionData = {};
 
+// Store name of the button in sessionStorage
 function storeChoice() {
-    let buttonName = this.name;
-    console.log("Button name:", buttonName);
-    sessionStorage.setItem("buttonName", buttonName);
+    // let buttonName = this.name;
+    let number = 0;
+
+    sessionStorage.setItem(`buttonName${number}`, this.name);
+    console.log(this.name);
+
+    // endArray.push(sessionStorage.getItem("buttonName"));
+    number++;
+}
+
+function returnChoices() {
+    // Loop through all keys in sessionStorage
+    for (let i = 0; i < sessionStorage.length; i++) {
+        // Get the key at the current index
+        let key = sessionStorage.key(i);
+
+        // Get the value associated with that key
+        let value = sessionStorage.getItem(key);
+
+        // Store it in the sessionData object
+        sessionData[key] = value;
+    }
 }
 
 const buttons = document.querySelectorAll("button");
